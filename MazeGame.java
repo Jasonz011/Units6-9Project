31da8e4900ffbf2c0e7c114526edf4e4d162d2d8
BT_2N2_15 <sample@bths.edu>
2024-03-15 13:25:21 -0400

    created play method

--- MazeGame.java
+++ MazeGame.java
@@ -1,3 +1,5 @@
+import java.util.Locale;
+
 public class MazeGame {
     private Space[][] maze;
     public MazeGame() {
@@ -5,6 +7,64 @@ public class MazeGame {
     }
 
     public void start() {
+
+    }
+
+    public void play() {
+        int row = 7;
+        int col = 0;
+        boolean game = true;
+        while (game) {
+            printMaze();
+            System.out.print("Enter W A S D: ");
+            String choice = scanner.nextLine().toLowerCase();
+            if (choice.equals("w")) {
+                if (row > 0) {
+                    maze[row][col] = new Space(" ");
+                    row--;
+                    p1.move();
+                } else {
+                    System.out.println("Will go out of bound");
+                }
+            } else if (choice.equals("a")) {
+                if (col > 0) {
+                    maze[row][col] = new Space(" ");
+                    col--;
+                    p1.move();
+                } else {
+                    System.out.println("Will go out of bound");
+                }
+            } else if (choice.equals("s")) {
+                if (row < 7) {
+                    maze[row][col] = new Space(" ");
+                    row++;
+                    p1.move();
+                } else {
+                    System.out.println("Will go out of bound");
+                }
+            } else if (choice.equals("d")) {
+                if (col < 7) {
+                    maze[row][col] = new Space(" ");
+                    col++;
+                    p1.move();
+                } else {
+                    System.out.println("Will go out of bound");
+                }
+            } else {
+                System.out.println("INVALID DIRECTION");
+            }
+            if (maze[row][col] instanceof Coin)  {
+                p1.addCoins(((Coin) maze[row][col]).getCoinValue());
+                System.out.println("You picked a treasure valued at " + ((Coin) maze[row][col]).getCoinValue());
+            } else if (maze[row][col] instanceof Goal) {
+                game = false;
+            }
+            maze[row][col] = p1;
+        }
+        printMaze();
+        System.out.println("You win!");
+        System.out.println("Number of moves: " + p1.getMoves());
+        System.out.println("Treasure points earned: " + p1.getScore());
 
     }
 }
--- Player.java
+++ Player.java
@@ -1,15 +1,21 @@
 public class Player extends Space {
     private int score;
+    private int move;
     public Player(String s) {
         super(s);
         score = 0;
+        move = 0;
     }
 
     public int getScore() {
         return score;
     }
 
-    public void setScore(int newScore) {
-        score = newScore;
+    public void addCoins(int coins)  {
+        score += coins;
+    }
+
+    public void move() {
+        move++;
     }
 }
